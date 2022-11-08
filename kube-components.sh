4ceb2a0257ff513813c6a718ce823b741ab8dfbb
Firas Chbiki <35631117+Ferios08@users.noreply.github.com>
2022-11-08 12:01:32 +0100

    Create kube-components.sh

---
+++ kube-components.sh
@@ -0,0 +1,23 @@
+# Install Dependancies
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+# Install Key and add repo
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Install packages
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# Initiate Cluster
+sudo kubeadm init --cri-socket=unix:///var/run/cri-dockerd.sock
+
+#
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  
+# EOF
+  
